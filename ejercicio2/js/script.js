766f4296f2f48afd892be23fd668cb39f68d9027
MauricioAcosta <andres.taamap@gmail.com>
2018-04-15 23:03:07 -0500

    Titulos tabla en negrilla

--- ejercicio2/js/script.js
+++ ejercicio2/js/script.js
@@ -43,6 +43,8 @@ function create() {
   var table_end = "</table>";
   var tr_start = "<tr>";
   var tr_end = "</tr>";
+  var th_start = "<th>";
+  var th_end = "</th>";
   var td_start = "<td>";
   var td_end = "</td>";
   document.write(table_start);
@@ -51,22 +53,22 @@ function create() {
     document.write(tr_start);
     for (var c = 0; c < 6; c++) {
       if (r === 0 && c === 0) {
-        document.write(td_start + "Proceso" + td_end);
+        document.write(th_start + "Proceso" + th_end);
       }
       if (r === 0 && c === 1) {
-        document.write(td_start + "Tiempo de llegada" + td_end);
+        document.write(th_start + "Tiempo de llegada" + th_end);
       }
       if (r === 0 && c === 2) {
-        document.write(td_start + "Tiempo de Rafaga" + td_end);
+        document.write(th_start + "Tiempo de Rafaga" + th_end);
       }
       if (r === 0 && c === 3) {
-        document.write(td_start + "Tiempo de retorno" + td_end);
+        document.write(th_start + "Tiempo de retorno" + th_end);
       }
       if (r === 0 && c === 4) {
-        document.write(td_start + "Tiempo de espera" + td_end);
+        document.write(th_start + "Tiempo de espera" + th_end);
       }
       if (r === 0 && c === 5) {
-        document.write(td_start + "Tiempo Final" + td_end);
+        document.write(th_start + "Tiempo Final" + th_end);
       }
       if (r >= 1 && c === 0) {
         document.write(td_start + "P" + r + td_end);
