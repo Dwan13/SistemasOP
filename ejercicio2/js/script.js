84fe7e9592fb2a494cf1aa74c964dcc9b9068252
MauricioAcosta <andres.taamap@gmail.com>
2018-04-15 20:04:23 -0500

    Se generan tiempos de espera y rafaga aleatorio

--- ejercicio2/js/script.js
+++ ejercicio2/js/script.js
@@ -1,16 +1,34 @@
+var timellegada = [0];
+var timeRafaga = [];
+var timeRetorno = [];
+var timeEspera = [];
+var timeFinal = [];
+var comodin = 0;
+var row = Math.floor((Math.random() * 100) + 1);
+
+function createTime() {
+  //create timellegada
+  for (var i = 1; i < row; i++) {
+    comodin = comodin + (i * Math.floor((Math.random() * 10) + 1))
+    timellegada.push(comodin)
+  }
+  //create timeRafaga
+  for (var i = 0; i < row; i++) {
+    timeRafaga.push(i * Math.floor((Math.random() * 10) + 1))
+  }
+}
 
 function create() {
-  mostrar=document.getElementById("Mostrar");
+  createTime()
   var table_start = "<table id=myTable border=1>";
   var table_end = "</table>";
   var tr_start = "<tr>";
   var tr_end = "</tr>";
   var td_start = "<td>";
   var td_end = "</td>";
-  var row = Math.floor((Math.random() * 100) + 1);
   document.write(table_start);
 
-  for (var r = 0; r < 10; r++) {
+  for (var r = 0; r < row; r++) {
     document.write(tr_start);
     for (var c = 0; c < 6; c++) {
       if (r === 0 && c === 0) {
@@ -31,8 +49,20 @@ function create() {
       if (r === 0 && c === 5) {
         document.write(td_start + "Tiempo Final" + td_end);
       }
-      if (r>=1) {
-        document.write(td_start + "Datos" + td_end);
+      if (r >= 1) {
+        if (c === 0) {
+          document.write(td_start + "P" + r + td_end);
+        } else {
+          if (r === 1 && c === 1) {
+            document.write(td_start + timellegada[0] + " segundos" + td_end);
+          } else if (r > 1 && c === 1) {
+            document.write(td_start + timellegada[r] + " segundos" + td_end);
+          } else if (c === 2) {
+            document.write(td_start + timeRafaga[r] + " segundos" + td_end);
+          } else {
+            document.write(td_start + r + " " + c + td_end);
+          }
+        }
       }
     }
     document.write(tr_end);
