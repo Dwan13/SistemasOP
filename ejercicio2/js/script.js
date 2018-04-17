d4b6c36f7d74c90a5d1f76ee9c17051cf8358adc
MauricioAcosta <andres.taamap@gmail.com>
2018-04-17 13:57:44 -0500

    se pinta en canvas cada segundo

--- ejercicio2/js/script.js
+++ ejercicio2/js/script.js
@@ -5,14 +5,18 @@ var timeEspera = [];
 var timeFinal = [];
 var comodin = 3;
 var espacio = 10;
-var table_start = "<table border=20>";
-var table_end = "</table>";
-var tr_start = "<tr>";
-var tr_end = "</tr>";
-var td_start = "<td>";
-var td_end = "</td>";
-
+var multiplo = 0;
+var tiempo = 1000; // un segundo
+var y = 0;
 var row = Math.floor(Math.random() * (10 - 2)) + 2;
+
+// //Hilo ejecuta cosas cada 1 segundos
+// for (var i = 0; i < 5; i++) {
+//   setTimeout((function() {
+//     console.log('Ejecuto cosas en el futuro (A) en indice: ', this)
+//   }).bind(i), multiplo * tiempo);
+//   multiplo += 1; // 1 porque es un procedimiento por for
+// }
 
 function createTime() {
   timellegada = [0];
@@ -28,35 +32,41 @@ function createTime() {
     comodin = comodin + Math.floor((Math.random() * 10) + 1) * i
     timellegada.push(comodin)
   }
-  console.log("timellegada ", timellegada);
+  // console.log("timellegada ", timellegada);
   //create timeRafaga
   for (var i = 0; i < row - 1; i++) {
     timeRafaga.push(Math.floor((Math.random() * 100) + 1))
   }
-  console.log("timeRafaga ", timeRafaga);
+  // console.log("timeRafaga ", timeRafaga);
 
   //create timeFinal
   timeFinal.push(timeRafaga[0])
   for (var i = 1; i < row - 1; i++) {
     timeFinal.push(timeRafaga[i] + timeFinal[i - 1])
   }
-  console.log("timeFinal ", timeFinal);
+  // console.log("timeFinal ", timeFinal);
   //create timeRetorno
   for (var i = 0; i < row - 1; i++) {
     timeRetorno.push(timeFinal[i] - timellegada[i])
   }
-  console.log("timeRetorno ", timeRetorno);
+  // console.log("timeRetorno ", timeRetorno);
   //create timeEspera
   for (var i = 0; i < row - 1; i++) {
     timeEspera.push(timeRetorno[i] - timeRafaga[i])
   }
-  console.log("timeEspera ", timeEspera);
+  // console.log("timeEspera ", timeEspera);
 }
 
 function createTable() {
   var a = document.body.childNodes
   document.body.removeChild(a[3]);
   var html = "";
+  var table_start = "<table border=20>";
+  var table_end = "</table>";
+  var tr_start = "<tr>";
+  var tr_end = "</tr>";
+  var td_start = "<td>";
+  var td_end = "</td>";
   html += table_start;
 
   for (var r = 0; r < row; r++) {
@@ -117,25 +127,29 @@ function createGraph() {
   ctx.moveTo(0, 0);
   ctx.lineTo(timeFinal[timeFinal.length - 1] + 4, 0);
   ctx.stroke();
-
   for (var i = 0; i < row; i++) {
-    ctx.beginPath();
-    ctx.lineWidth = "5";
-    ctx.strokeStyle = "#000000";
-    ctx.rect(timellegada[i] + timeEspera[i], espacio * i + 20, timeRafaga[i], 5);
-    ctx.stroke();
-    if (i > 0) {
+    setTimeout((function() {
+      console.log("HOla entre", this);
       ctx.beginPath();
       ctx.lineWidth = "5";
-      ctx.strokeStyle = "#dfe9eb";
-      ctx.rect(timellegada[i], espacio * i + 20, timeEspera[i], 5);
+      ctx.strokeStyle = "#000000";
+      ctx.rect(timellegada[y] + timeEspera[y], espacio * y + 20, timeRafaga[y], 5);
       ctx.stroke();
-    }
-    ctx.beginPath() //iniciar ruta
-    // ctx.strokeStyle="#ffffff"; //color externo
-    ctx.fillStyle = "#ffffff"; //color de relleno
-    ctx.font = "bold 10px arial"; //estilo de texto
-    ctx.fillText("P" + (i + 1), timellegada[i] + timeEspera[i] + 3, espacio * i + 25);
-    ctx.stroke();
+      if (i > 0) {
+        ctx.beginPath();
+        ctx.lineWidth = "5";
+        ctx.strokeStyle = "#dfe9eb";
+        ctx.rect(timellegada[y], espacio * y + 20, timeEspera[y], 5);
+        ctx.stroke();
+      }
+      ctx.beginPath() //iniciar ruta
+      // ctx.strokeStyle="#ffffff"; //color externo
+      ctx.fillStyle = "#ffffff"; //color de relleno
+      ctx.font = "bold 10px arial"; //estilo de texto
+      ctx.fillText("P" + (y + 1), timellegada[y] + timeEspera[y] + 3, espacio * y + 25);
+      ctx.stroke();
+      y += 1;
+    }).bind(i), multiplo * tiempo);
+    multiplo += 1; // 1 porque es un procedimiento por for
   }
 }
